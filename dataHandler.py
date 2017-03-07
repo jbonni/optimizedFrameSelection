1e94195755b8ceb7c40362420f00e6e8fb56442d
cgel <carlesgelada@hotmail.com>
2017-03-07 17:06:35 +0100

    solve dataset folder issue

--- dataHandler.py
+++ dataHandler.py
@@ -3,38 +3,38 @@ import os
 
 class dataHandler():
     def __init__(self):
-        self.data = {"frame"        :{}, 
-                     "qVal"         :{}, 
-                     "action"       :{}, 
+        self.data = {"frame"        :{},
+                     "qVal"         :{},
+                     "action"       :{},
                      "reward"       :{},
                      "isTerminal"   :{},
                      "internal1"    :{},
                      "internal2"    :{}}
         self.qty    = 0
-        
-        self.dir = os.path.join(os.path.dirname(__file__), '/dataset/')
-        
-        
+
+        self.dir = os.path.join(os.path.dirname(__file__), './dataset/')
+
+
     def emptyHandler(self):
         self.data.clear()
-        self.data = {"frame"        :{}, 
-                     "qVal"         :{}, 
-                     "action"       :{}, 
+        self.data = {"frame"        :{},
+                     "qVal"         :{},
+                     "action"       :{},
                      "reward"       :{},
                      "isTerminal"   :{},
                      "internal1"    :{},
                      "internal2"    :{}}
         self.qty = 0
-        
+
     def addData(self,
-                frame, 
-                expectedQ, 
-                action, 
-                reward, 
-                isTerminal, 
-                internal1, 
+                frame,
+                expectedQ,
+                action,
+                reward,
+                isTerminal,
+                internal1,
                 internal2):
-        
+
         self.data["frame"][self.qty]      = frame
         self.data["qVal"][self.qty]       = expectedQ
         self.data["action"][self.qty]     = action
@@ -43,23 +43,23 @@ class dataHandler():
         self.data["internal1"][self.qty]  = internal1
         self.data["internal2"][self.qty]  = internal2
         self.qty += 1
-        
-        if self.qty > 1000000:
+
+        if self.qty > 100:
             self.pickleSave()
-        
-    
+
+
     def pickleSave(self):
         if not os.path.exists(self.dir):
             os.makedirs(self.dir)
-            
+
         with open(self.dir + 'dataset.pickle', 'a+b') as handle:
             pickle.dump(self.data, handle, protocol=pickle.HIGHEST_PROTOCOL)
             self.emptyHandler()
-            
+
     def pickleLoadNext(self):
         try:
             self.data = pickle.load(self.pickleLoad)
-            self.qty    = len(self.data["isTerminal"]) 
+            self.qty    = len(self.data["isTerminal"])
             return True
         except EOFError:
             print("EOF")
