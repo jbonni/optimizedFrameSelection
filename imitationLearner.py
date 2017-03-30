bbfe069d40acbbc56e09acebe09b9412cb821840
jbonni <julien.bonnier.1@etsmtl.net>
2017-03-30 17:40:27 +0200

    Test with mnist. need cleanup

--- imitationLearner.py
+++ imitationLearner.py
@@ -1,22 +1,24 @@
 import tensorflow as tf
 import time
-import imitParam
+import param
 import dataHandler as dh
 import numpy as np
 from sklearn.neighbors import LSHForest
+import h5py
 
+import NeuralNetwork as nn
 
-
+tf.logging.set_verbosity(0)
 
 class imitationLearner():
-    def __init__(self, session, param = imitParam.imitParam):
+    def __init__(self, session, param = param.neuralParam):
         #learner session
         self.sess = session
         #parameters of the learner
         self.param = param
         #placeholders
         self.input_ph = tf.placeholder(
-                tf.float32, self.param.input_shape, name="inputData_ph")
+                tf.float32, self.param.flat_input_shape, name="inputData_ph")
         self.output_ph = tf.placeholder(
                 tf.float32, self.param.output_shape, name="output_ph")
         self.label_ph = tf.placeholder(
@@ -25,8 +27,10 @@ class imitationLearner():
                 tf.float32, name="dropout_ph")
 
         #Creates the learner and every operation necessary
-        self.learner = self.createLearner()
-        self.loss_op, _ = self.loss()
+        self.output_ph = nn.createLearner(self.input_ph,
+                                        self.keep_prob,
+                                        self.param)
+        self.loss_op = self.loss()
         self.train_op = self.training()
         self.eval_op = self.evaluation()
 
@@ -34,93 +38,49 @@ class imitationLearner():
         self.dh = dh.dataHandler()
 
 
-    def addConvLayer(self,inputs, shape):
-        return tf.layers.conv2d(inputs=inputs,
-                                filters=shape[0],
-                                kernel_size=[shape[1],\
-                                            shape[2]],
-                                padding="same",
-                                strides=shape[3],
-                                activation=tf.nn.relu)
-
-    def addDenseLayer(self,inputs, shape):
-        return tf.layers.dense(inputs=inputs,
-                               units=shape.pop(),
-                               activation=tf.nn.relu)
-
-
-    def createLearner(self, normalize = False):
-
-        if normalize:
-            input_norm = tf.div(self.input_ph, 255., name="normalized_input")
-        else:
-            input_norm = self.input_ph
-
-
-        #Input reshape
-        input_reshape_norm = tf.reshape(input_norm, [-1,28,28,1])#A mettre en parametres
-
-
-        #create all conv layers
-        convLayers = []
-        for index, shape in enumerate(self.param.Conv_param):
-
-            if index ==0:
-                #first conv layer
-                convLayers.append(self.addConvLayer(input_reshape_norm,shape))
-            else:
-                convLayers.append(self.addConvLayer(convLayers[index-1],shape))
-
-        #index of the last conv layer
-        lastConvLayerIndex = len(self.param.Conv_param)-1
-
-        #conv to tense Layer conversion
-        outConv = convLayers[lastConvLayerIndex]
-        outShape = outConv.get_shape().as_list()
-        lastConvFlat = tf.reshape(outConv,
-                                [-1, outShape[1]*outShape[2]*outShape[3]])
-
-        #create the dense layers
-        denseLayers = []
-        for index, shape in enumerate(self.param.fully_connected_layer):
-            if index == 0:
-                denseLayers.append(self.addDenseLayer(lastConvFlat,
-                                                      shape))
-            else:
-                denseLayers.append(self.addDenseLayer(denseLayers[index-1],
-                                                      shape))
-        #index of the last
-        lastDenseLayerIndex = len(self.param.fully_connected_layer)-1
-
-        #drouput layer
-        drop = tf.nn.dropout(denseLayers[lastDenseLayerIndex], self.keep_prob)
-
-        #Output Layer
-        self.output_ph = tf.layers.dense(inputs=drop,
-                                         units=self.param.output_shape[1])
-
-
-
-
-
     def loss(self):
-        loss_op = tf.nn.softmax_cross_entropy_with_logits(
-                                                      labels=self.label_ph,
-                                                      logits=self.output_ph)
-        return loss_op, tf.reduce_mean(loss_op)
+#        loss_op = tf.losses.log_loss(self.output_ph,self.label_ph)
+        if self.param.loss == "mse":
+            loss_op = tf.reduce_sum(tf.square(self.output_ph - self.label_ph))
+#        elif self.param.loss == "cos":
+#            loss_op = tf.losses.cosine_distance(self.label_ph,
+#                                                self.output_ph)
+#        elif self.param.loss == "hinge":
+#            loss_op = tf.losses.hinge_loss(self.label_ph,
+#                                           self.output_ph)
+        elif self.param.loss == "softmax":
+            loss_op = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(
+                                                      labels=tf.nn.softmax(self.label_ph),
+                                                      logits=tf.nn.softmax(self.output_ph)))
+#        elif self.param.
+        else:
+            print('loss function not defined')
+            raise
+#
+##        tf.losses.log_loss(self.output_ph,
+##                                     self.label_ph,
+##                                     weights=2.0,
+##                                     epsilon = 0.01)
 
+#        loss_op = tf.nn.softmax_cross_entropy_with_logits(
+#                                                      labels=tf.nn.softmax(self.label_ph),
+#                                                      logits=self.output_ph)
+        return loss_op
 
 
 
     def training(self):
         # Add a scalar summary for the snapshot loss.
-        tf.summary.scalar('loss', tf.reduce_mean(self.loss_op))
+#        tf.summary.scalar('loss', tf.reduce_mean(self.loss_op))
         # Create the gradient descent optimizer with the given learning rate.
         optimizer = tf.train.GradientDescentOptimizer(self.param.learning_rate)
         # Create a variable to track the global step.
+#        train_op = tf.train.AdamOptimizer(self.param.learning_rate).minimize(self.loss_op)
+
+#        train_op = tf.train.RMSPropOptimizer(self.param.learning_rate).minimize(self.loss_op)
         global_step = tf.Variable(0, name='global_step', trainable=False)
-        # Use the optimizer to apply the gradients that minimize the loss
-        #(and also increment the global step counter) as a single training step.
+#        # Use the optimizer to apply the gradients that minimize the loss
+#        #(and also increment the global step counter) as a single training step.
         train_op = optimizer.minimize(self.loss_op, global_step=global_step)
         return train_op
 
@@ -136,8 +96,50 @@ class imitationLearner():
 
     def run(self):
         #Loads the MNIST dataset
-        self.dh.setFilename("mnist.hdf5")
+        print("run")
+        self.dh.setFilename("mnist.hdf5")#--------------modify
+        testBatch = self.dh.loadBatch("test")
+        lsh = LSHForest(min_hash_match=8, n_candidates=10, n_estimators=50,
+          n_neighbors=10, radius=10.0, radius_cutoff_ratio=0.5,
+          random_state=42)
         dhGen = dh.dataHandler()
+
+        total = self.dh.loadRange(0,70000)
+#        print(total)
+        total = total[:][0:794]
+        lsh.fit(total)
+
+        for i, data in enumerate(total):
+            distances, indices =  lsh.kneighbors(data[50], n_neighbors=2)
+    #        print(indices)
+    #        print(distances)
+            if distances[0,0] < 1.1e-15:
+                pass#identical in mem
+
+            if distances[0,1] < 2.0e-01:
+                pass#close in mem
+            else:
+                dhGen.addData(data)
+
+        raise
+
+        #separate data
+        dataShape = self.dh.getDataShape()
+        testData = testBatch[:,dataShape[0,0]:dataShape[0,1]]
+        testLabel = testBatch[:,dataShape[1,0]:dataShape[1,1]]
+#        self.dh.setFilename("rl_data.hdf5")
+#        with h5py.File(self.dh.fileName, "r",  libver='latest') as f:
+#            print("openFile")
+#            testList = f["batch/test/list"]
+#            testBatch = self.dh.load(testList[0:2000])
+#            dataShape = self.dh.getDataShape()
+#            testData = testBatch[:,dataShape[0,0]:dataShape[0,1]]
+#            testLabel = testBatch[:,dataShape[1,0]:dataShape[1,1]]
+
+#        dhGen = dh.dataHandler()
+#        dataShape = self.dh.getDataShape()
+#        print(dataShape)
+#        raise
 
         # Add the variable initializer Op.
         init = tf.global_variables_initializer()
@@ -147,53 +149,59 @@ class imitationLearner():
 
         #load the test batch
         print(" ".join(["loading",self.dh.fileName]))
-        testBatch = self.dh.loadBatch("test")
+#        testBatch = self.dh.loadBatch("test")
 
-        #separate data
-        dataShape = self.dh.getDataShape()
-        testData = testBatch[:,dataShape[0,0]:dataShape[0,1]]
-        testLabel = testBatch[:,dataShape[1,0]:dataShape[1,1]]
 
         #Training loop
         print("Begin training")
-        start_time = time.time()
+#        start_time = time.time()
         for step in range(self.param.max_steps):
-
-
             batch = self.dh.loadBatch()
+
 
             feed_dict = {self.input_ph:batch[:,dataShape[0,0]:dataShape[0,1]],
                          self.label_ph:batch[:,dataShape[1,0]:dataShape[1,1]],
                          self.keep_prob: 0.5}
 
-            # Run one step of the model.
-            _, loss = self.sess.run([self.train_op, self.loss_op],
-                                          feed_dict=feed_dict)
 
-            if step >= 20000:
-                self.param.learning_rate = 0
-                for data in batch[:,dataShape[0,0]:dataShape[0,1]]:
-                    dhGen.addData(data, loss)
+            # Run one step of the model.
+            _, loss = self.sess.run([self.train_op,self.loss_op],
+                                    feed_dict=feed_dict)
+
+
+
+
+
+
+#
+#            if step >= 30000:
+#                for d, l in zip(batch[:,dataShape[0,0]:dataShape[0,1]],loss):
+#                    dhGen.addData(d, l)
 
             #Show loss of network
-            if step % 100 == 0:
-              duration = time.time() - start_time
-              print('Step {0}: loss = {1:.3f} ({2:.3f} sec)'.format(step, np.mean(loss), duration))
-              start_time = time.time()
+#            if step % 10 == 0:
+#              duration = time.time() - start_time
+#              print('Step {0}: loss = {1:.10f} ({2:.3f} sec)'.format(step, loss, duration))
+#              start_time = time.time()
 
 
             #Evaluate the model periodically.
-            if (step + 1) % 2500 == 0 or (step + 1) == self.param.max_steps:
-                print("test accuracy %g"%self.sess.run(self.eval_op,
+            if (step + 1) % 100 == 0 or (step + 1) == self.param.max_steps:
+                print("%g"%self.sess.run(self.eval_op,
                                                   feed_dict={
                                             self.input_ph: testData,
                                             self.label_ph: testLabel,
                                             self.keep_prob: 1.0}))
-                start_time = time.time()
-        dhGen.saveData()
+#                start_time = time.time()
+#        dhGen.saveData()
+#        tr, tst = dhGen.randList(64)
+#        dhGen.createBatch(tr,"training")
+##        dhGen.createBatch(tst,"test")
+#        return dhGen
+
 
 
 if __name__ == "__main__":
     sess = tf.Session()
     imit = imitationLearner(sess)
-    imit.run()
+    d = imit.run()
